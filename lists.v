addb9ae3caf1aa14ec485dae5391f8566378d41b
SmnTin <smn.pankv@gmail.com>
2022-10-30 19:53:55 +0100

    Formalize `nth` and `length`

--- lists.v
+++ lists.v
@@ -41,3 +41,41 @@ Section app.
 End app.
 
 Arguments app {A} xs ys.
+
+Fixpoint length {A : Type} (xs : list A) : nat :=
+  match xs with
+  | [] => 0
+  | _ :: xs => S (length xs)
+  end.
+
+Section nth.
+  Variable A : Type.
+
+  Fixpoint nth (n : nat) (xs : list A) (default : A) : A :=
+    match n, xs with
+    | 0, x :: _ => x
+    | _, [] => default
+    | S n, _ :: xs => nth n xs default
+    end.
+
+  Fixpoint nth_ok (n : nat) (xs : list A) : bool :=
+    match n, xs with
+    | 0, _ :: _ => true
+    | _, [] => false
+    | S n, _ :: xs => nth_ok n xs
+    end.
+
+  Lemma nth_ok_ok n xs :
+    n < length xs -> nth_ok n xs = true.
+  Proof.
+    generalize dependent n.
+    induction xs as [| x xs IH].
+    - intros n H. inversion H.
+    - intros [| n] H.
+      + simpl. reflexivity.
+      + simpl in *. apply IH. apply le_S_n. apply H.
+  Qed.
+End nth.
+
+Arguments nth {A}.
+Arguments nth_ok {A}.
